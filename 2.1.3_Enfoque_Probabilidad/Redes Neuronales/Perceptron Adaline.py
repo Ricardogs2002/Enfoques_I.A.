3e93c6832a498d748a476b03cce9a80da8fc772f
Sharlo RDT <124947231+Sharliee@users.noreply.github.com>
2023-05-07 18:18:17 -0600

    Add files via upload

---
+++ 2.1.3_Enfoque_Probabilidad/Redes Neuronales/Perceptron Adaline.py
@@ -0,0 +1,51 @@
+#Carlos Andres Riveramelo Del Toro
+#Perceptrone Adaline y Madaline "ADALINE"
+
+#Practicamente son lo mismo, solo que el adeline es una red neuronal de 1 sola capa
+#y el madeline funciona con multiples capas
+
+import numpy as np
+
+class Adaline:
+    def __init__(self, learning_rate=0.01, epochs=50):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        #Inicializa los pesos y el sesgo aleatoriamente
+        self.weights = np.random.randn(X.shape[1])
+        self.bias = np.random.randn()
+
+        for _ in range(self.epochs):
+            #Calcula la salida y la diferencia de error
+            output = self.predict(X)
+            error = y - output
+
+            #Actualiza los pesos y el sesgo
+            self.weights += self.learning_rate * np.dot(X.T, error)
+            self.bias += self.learning_rate * np.sum(error)
+
+    def predict(self, X):
+        #Calcula la salida
+        return np.dot(X, self.weights) + self.bias
+
+#Datos de entrenamiento
+X = np.array([[0.5, 0.2],
+              [0.3, 0.8],
+              [0.9, 0.5],
+              [0.1, 0.9]])
+y = np.array([0.4, 0.6, 0.8, 0.2])
+
+#Crea y entrena el modelo Adaline
+model = Adaline(learning_rate=0.01, epochs=100)
+model.fit(X, y)
+
+#Datos de prueba
+X_test = np.array([[0.4, 0.3],
+                   [0.7, 0.6]])
+
+#Realiza predicciones
+predictions = model.predict(X_test)
+print("Predicciones:", predictions)
---
+++ 2.1.3_Enfoque_Probabilidad/Redes Neuronales/Perceptron Madaline.py
@@ -0,0 +1,70 @@
+#Carlos Andres Riveramelo Del Toro
+#Perceptrone Adaline y Madaline "MADALINE"
+
+#Practicamente son lo mismo, solo que el adeline es una red neuronal de 1 sola capa
+#y el madeline funciona con multiples capas
+
+#Este programa resuelve un problema de clasificacion binaria
+
+import numpy as np
+
+class Madaline:
+    def __init__(self, num_layers, layer_size, learning_rate=0.01, epochs=50):
+        self.num_layers = num_layers
+        self.layer_size = layer_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+
+    def fit(self, X, y):
+        #Inicializa los pesos aleatoriamente
+        self.weights = [np.random.randn(X.shape[1], self.layer_size)]
+        self.weights += [np.random.randn(self.layer_size, self.layer_size) for _ in range(self.num_layers - 1)]
+
+        for _ in range(self.epochs):
+            for xi, target in zip(X, y):
+                #Propagación hacia adelante
+                outputs = [xi]
+                for i in range(self.num_layers):
+                    net_input = np.dot(outputs[i], self.weights[i])
+                    output = self._linear_activation(net_input)
+                    outputs.append(output)
+
+                #Retropropagación del error y ajuste de los pesos
+                errors = [target - outputs[-1]]
+                for i in range(self.num_layers - 1, 0, -1):
+                    error = np.dot(errors[0], self.weights[i].T)
+                    errors.insert(0, error)
+                    delta = self.learning_rate * errors[0] * self._linear_derivative(outputs[i])
+                    self.weights[i] += outputs[i].reshape(-1, 1) @ delta.reshape(1, -1)
+
+    def predict(self, X):
+        #Propagación hacia adelante para realizar predicciones
+        outputs = [X]
+        for i in range(self.num_layers):
+            net_input = np.dot(outputs[i], self.weights[i])
+            output = self._linear_activation(net_input)
+            outputs.append(output)
+
+        return np.where(outputs[-1] >= 0.5, 1, 0)
+
+    def _linear_activation(self, x):
+        return x
+
+    def _linear_derivative(self, x):
+        return 1
+
+#Datos de entrenamiento
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
+
+#Crea y entrenar el modelo Madaline
+model = Madaline(num_layers=2, layer_size=2, learning_rate=0.01, epochs=100)
+model.fit(X, y)
+
+#Datos de prueba
+X_test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+#Realiza predicciones
+predictions = model.predict(X_test)
+print("Predicciones:", predictions)
