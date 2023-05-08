e26e8c7d64ef0ef81ab535040b2692f273a0831b
Adan Alvarez <a20310442@live.ceti.mx>
2023-05-07 18:41:45 -0600

    0107_Adan_Alvarez_Barajas

---
+++ 2.1.4_Enfoque_Logica/Otras Lógicas/0002_Logica_Modal.py
@@ -0,0 +1,32 @@
+"""
+Created on Sun Apr 7 2023
+
+@author: Adan Alvarez
+"""
+
+import random
+import time
+
+# Establecemos el umbral de cambio de estado, porcentaje de tendencia
+umbral = 0.8      #Reducir para aumentar los eventos
+
+# Estado inicial
+estado = "No se detecta movimiento"
+
+# Ciclo principal
+while True:
+    # Generamos un número aleatorio
+    aleatorio = random.random()
+
+    # Verificamos si cambiamos de estado
+    if aleatorio > umbral:
+        estado = "Movimiento detectado!!!             Encendiendo luces                  Activando Alarma    "
+    else:
+        estado = "No se detecta movimiento"
+
+    # Imprimimos el estado actual
+    print(estado)
+
+    # Esperamos un segundo antes de continuar
+    time.sleep(1)
+
