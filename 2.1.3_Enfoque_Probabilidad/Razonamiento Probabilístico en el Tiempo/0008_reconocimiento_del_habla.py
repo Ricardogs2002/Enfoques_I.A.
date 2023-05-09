79010f1a9f6a5fced054968808948ae8b7b98d8f
Sharlo RDT <124947231+Sharliee@users.noreply.github.com>
2023-05-09 17:03:33 -0600

    Add files via upload

---
+++ 2.1.3_Enfoque_Probabilidad/Razonamiento Probabilístico en el Tiempo/0008_reconocimiento_del_habla.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 14:18:54 2023
+
+@author: Alberto Aguiar
+"""
+
+import speech_recognition as sr
+
+# Inicializar el reconocedor de voz
+r = sr.Recognizer()
+
+# Utilizar el micrófono como fuente de audio
+mic = sr.Microphone()
+
+# Escuchar el audio desde el micrófono
+
+with mic as source:
+    
+    audio = r.listen(source)
+
+
+# Realizar reconocimiento de voz utilizando el servicio de reconocimiento de Google
+texto_transcrito = r.recognize_google(audio, language = 'ES')
+
+# Imprimir el resultado de la transcripción
+print(f'Has dicho: {texto_transcrito}')
