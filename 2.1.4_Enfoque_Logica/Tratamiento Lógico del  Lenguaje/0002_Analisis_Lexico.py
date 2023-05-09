ccd178692aa4cf25ae6d17dde4778ec159c2bd15
Adan Alvarez <a20310442@live.ceti.mx>
2023-05-09 01:05:28 -0600

    0143_Adan_Alvarez_Barajas

---
+++ 2.1.4_Enfoque_Logica/Tratamiento Lógico del  Lenguaje/0002_Analisis_Lexico.py
@@ -0,0 +1,23 @@
+"""
+Created on Mon May 8 2023
+
+@author: Adan Alvarez
+"""
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# lista de preguntas
+preguntas = ["¿Que es la Inteligencia Artificial?"]
+
+# Crea el objeto de vectorizador TfidfVectorizer
+vectorizer = TfidfVectorizer()
+
+# Convierte el texto en una matriz de características numericas basada en la
+# frecuencia de los términos y la importancia inversa del documento (TF-IDF).
+vector_preguntas = vectorizer.fit_transform(preguntas)
+
+# imprimir la matriz de pesos
+print("\n", vector_preguntas.toarray())
+
+# imprimir la lista de palabras en el orden de los pesos
+print("\n", vectorizer.get_feature_names_out())
