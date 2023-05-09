ba602f7d0e6e28726f39dbab006981e567e5bb39
Sharlo RDT <124947231+Sharliee@users.noreply.github.com>
2023-05-09 17:04:46 -0600

    Add files via upload

---
+++ 2.1.4_Enfoque_Logica/Lógica de 1er Orden/0008_agentes_logicos.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 14:49:20 2023
+
+@author: Alberto Aguiar
+"""
+
+from pyDatalog import pyDatalog
+
+# Definir las reglas y hechos lógicos
+pyDatalog.create_terms('padre, madre, abuelo, hermano, hermana, X, Y, Z')
+
++padre('Juan', 'Pedro')
++madre('María', 'Pedro')
++padre('Pedro', 'Luis')
++madre('Ana', 'Luis')
+
+abuelo(X, Y) <= padre(X, Z) & padre(Z, Y)
+hermano(X, Y) <= padre(Z, X) & padre(Z, Y) & (X != Y)
+hermana(X, Y) <= madre(Z, X) & madre(Z, Y) & (X != Y)
+
+# Consultas
+print("Abuelos:")
+print(abuelo(X, Y))
+print()
+
+print("Hermanos:")
+print(hermano(X, Y))
+print()
+
+print("Hermanas:")
+print(hermana(X, Y))
