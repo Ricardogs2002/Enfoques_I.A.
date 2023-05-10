4c85ab20d46c3ffb5f36c8788e3c26561d486c25
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-05-09 20:36:08 -0600

    Update 0006_busqueda_bidireccional.py

--- 2.1.2_Enfoque_Grafos/Busqueda_no_informada/0006_busqueda_bidireccional.py
+++ 2.1.2_Enfoque_Grafos/Busqueda_no_informada/0006_busqueda_bidireccional.py
@@ -1,1 +1,36 @@
+from queue import Queue
 
+# Crear una nueva cola
+q = Queue()
+
+# Agregar elementos a la cola
+q.put('A')
+q.put('B')
+q.put('C')
+
+# Obtener el primer elemento de la cola y eliminarlo de la cola
+item = q.get()
+print(item)  # Resultado: 'A'
+
+# Verificar si la cola está vacía
+is_empty = q.empty()
+print(is_empty)  # Resultado: False
+
+# Obtener el primer elemento de la cola sin eliminarlo de la cola
+item = q.queue[0]
+print(item)  # Resultado: 'B'
+
+# Obtener el número de elementos en la cola
+size = q.qsize()
+print(size)  # Resultado: 2
+
+"""
+La búsqueda bidireccional es una técnica utilizada en la 
+inteligencia artificial para encontrar una solución óptima 
+a un problema, comienza la búsqueda desde el estado inicial y 
+el estado objetivo al mismo tiempo, utilizando dos conjuntos de 
+nodos: uno desde el estado inicial y otro desde el estado objetivo. 
+A medida que la búsqueda avanza, los dos conjuntos de nodos se 
+expanden hacia adelante hasta que se encuentran en algún punto intermedio. 
+
+"""
