d05363a8db3b138941abf06454df288c5272d8bb
D-714 <124953914+Ricardogs2002@users.noreply.github.com>
2023-05-09 19:17:57 -0600

    0179_Ricardo_Ismael_Gomez_Sanchez
    Ajuste de programa de anchura

--- 2.1.2_Enfoque_Grafos/Busqueda_no_informada/0001_busqueda_anchura.py
+++ 2.1.2_Enfoque_Grafos/Busqueda_no_informada/0001_busqueda_anchura.py
@@ -4,11 +4,168 @@ Created on Sun Apr 23 19:27:54 2023
 @author: 6E1
 """
 
-# Importamos las clases necesarias para el problema
-from Clases_base_Grafos import Accion
-from Clases_base_Grafos import Estado
-from Clases_base_Grafos import Nodo
-from Clases_base_Grafos import Problema
+#%%Acción
+class Accion:
+    def __init__(self,nombre):
+        self.nombre=nombre
+        
+    def __str__(self):
+        return self.nombre
+
+
+#%%Estado
+class Estado:
+    def __init__(self,nombre,acciones):
+        self.nombre=nombre
+        self.acciones=acciones
+        
+    def __str__(self):
+        return self.nombre
+
+
+#%%Problema
+class Problema:
+    def __init__(self,estado_inicial,estados_objetivos,acciones,
+                 costes=None,heuristicas=None):
+        self.estado_inicial=estado_inicial
+        self.estados_objetivos=estados_objetivos
+        self.acciones=acciones
+        self.costes=costes
+        self.heuristicas=heuristicas
+        self.infinito=99999
+        if not self.costes:
+            self.costes={}
+            for estado in self.acciones.keys():
+                self.costes[estado]={}
+                for accion in self.acciones[estado].keys():
+                    self.costes[estado][accion]=1
+        if not self.heuristicas:
+            self.heuristicas={}
+            for estado in self.acciones.keys():
+                self.heuristicas[estado]={}
+                for objetivo in self.estados_objetivos:
+                    self.heuristicas[estado][objetivo]=self.infinito
+
+    def __str__(self):
+        msg="Estado Inicial: {0} -> Objetivos: {1}"
+        return msg.format(self.estado_inicial.nombre,
+                          self.estados_objetivos)
+    
+    def es_objetivo(self,estado):
+        return estado in self.estados_objetivos
+    
+    def resultado(self,estado,accion):
+        if estado.nombre not in self.acciones.keys():
+            return None
+        acciones_estado=self.acciones[estado.nombre]
+        if accion.nombre not in self.acciones[estado.nombre]:
+            return None
+        return acciones_estado[accion.nombre]
+    
+    def coste_accion(self,estado,accion):
+        if estado.nombre not in self.costes.keys():
+            return self.infinito
+        costes_estado=self.costes[estado.nombre]
+        if accion.nombre not in costes_estado.keys():
+            return self.infinito
+        return costes_estado[accion.nombre]
+    
+    def coste_camino(self,nodo):
+        total=0
+        while nodo.padre:
+            total+=self.coste_accion(nodo.padre.estado, nodo.accion)
+            nodo=nodo.padre
+        return total
+
+
+#%%Nodo
+class Nodo:
+    def __init__(self,estado,accion=None,acciones=None,padre=None):
+        self.estado=estado
+        self.accion=accion
+        self.acciones=acciones
+        self.padre=padre
+        self.hijos=[]
+        self.coste=0
+        self.heuristicas={}
+        self.valores={}
+        self.alfa=0
+        self.beta=0
+        
+    def __str__(self):
+        return self.estado.nombre
+    
+    def expandir(self,problema):
+        self.hijos= []
+        if not self.acciones:
+            if self.estado.nmbre not in problema.acciones.keys():
+                return self.hijos
+            self.acciones=problema.acciones[self.estado.nombre]
+        for accion in self.acciones.keys():
+            accion_hijo=Accion(accion)
+            nuevo_estado=problema.resultado(self.estado,accion_hijo)
+            acciones_nuevo={}
+            if nuevo_estado.nobre in problema.acciones.keys():
+                acciones_nuevo=problema.acciones[nuevo_estado.nombre]
+            hijo=Nodo(nuevo_estado,accion_hijo,acciones_nuevo,self)
+            coste=self.padre.coste if self.padre else 0
+            coste+=problema.coste_accion(self.estado,accion_hijo)
+            hijo.coste=coste
+            hijo.heuristicas=problema.heuristicas[hijo.estado.nombre]
+            hijo.valores={estado:heuristica+hijo.coste
+                          for estado,heuristica
+                          in hijo.heuristicas.items()}
+            self.hijos.append(hijo)
+        return self.hijos
+    
+    def hijo_mejor(self,problema,metrica='valor',criterio='menor'):
+        if not self.hijos:
+            return None
+        mejor=self.hijos[0]
+        for hijo in self.hijos:
+            for objetivo in problema.estados_objetivos:
+                if metrica=='valor':
+                    valor_hijo=hijo.valores[objetivo.nombre]
+                    valor_mejor=mejor.valores[objetivo.nombre]
+                    if(criterio=='menor' and
+                       valor_hijo<valor_mejor):
+                        mejor=hijo
+                    elif(criterio=='mayor' and
+                         valor_hijo>valor_mejor):
+                        mejor=hijo
+                elif metrica=='heuristica':
+                    heuristica_hijo=hijo.heuristicas[objetivo.nombre]
+                    heuristica_mejor=mejor.heuristicas[objetivo.nombre]
+                    if(criterio=='menor' and
+                       heuristica_hijo<heuristica_mejor):
+                        mejor=hijo
+                    elif(criterio=='mayor' and
+                         heuristica_hijo>heuristica_mejor):
+                        mejor=hijo
+                elif metrica=='coste':
+                    coste_camino_hijo=problema.coste_camino(hijo)
+                    coste_camino_mejor=problema.coste_camino(mejor)
+                    if(criterio=='menor' and
+                       coste_camino_hijo<coste_camino_mejor):
+                        mejor=hijo
+                    elif(criterio=='mayor' and
+                       coste_camino_hijo>coste_camino_mejor):
+                        mejor=hijo
+                    elif metrica=='alfa':
+                        if(criterio=='menor' and
+                           hijo.alfa<mejor.alfa):
+                            mejor=hijo
+                        elif(criterio=='mayor' and
+                             hijo.alfa>mejor.alfa):
+                            mejor=hijo
+                    elif metrica=='beta':
+                        if(criterio=='menor' and
+                           hijo.beta<mejor.beta):
+                            mejor=hijo
+                        elif(criterio=='mayor' and
+                             hijo.beta>mejor.beta):
+                            mejor=hijo
+        return mejor
 
 #%%
 def anchura(problema):
